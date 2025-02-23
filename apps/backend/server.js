e2d36360090cc66abe2edc140f46b3607f86df53
Muhammad Zain <83939353+Muhammad-Zain01@users.noreply.github.com>
2025-02-23 19:35:30 +0500

    Updates

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -1,1 +1,29 @@
-console.log("This is server of the backend")
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+});
+
+const PORT = process.env.PORT || 3200;
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+server.listen(PORT, () => {
+  console.log("Server is running on PORT:", PORT);
+});
